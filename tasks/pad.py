2b56554c061f939dbe4abcd134c6c9f66698e75d
root <root@DESKTOP-I80IARA.localdomain>
2020-12-15 20:03:26 +0100

    Group8 Task: pad.py

--- tasks/pad.py
+++ tasks/pad.py
@@ -1,15 +1,37 @@
+import sys
+import numpy as np
 
 
 def pad(input_list):
-    """
-    Given a input list of nested list of integers:
+	"""
+input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
+return a matrix where
+1) matrix[i][:len(input_list[i])] == input_list[i]
+and
+2) matrix[i][len(input_list[i]):] == [0,0, ..., 0]
+... 
+"""
 
-    input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
+	input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
+	y=np.matrix(input_list)
+	length = 2*max(map(len, input_list))
+	matrix = np.matrix([x+[0]*(length-len(x)) for x in input_list], dtype="int")
 
-    return a matrix where 
-    1) matrix[:len(input_list[i])] == input_list[i]
-    and
-    2) matrix[len(input_list[i]):] == [0,0, ..., 0]
+	#matrix1= np.split(matrix,4)
+	#for i in range(len(input_list)):
+	#	if np.any(matrix1[i][:len(input_list[i])]) == np.any(input_list[i]):
+	#		print(True)
+	return matrix
+	
+					
 
-    """
-    pass
+
+
+    #pass
+
+
+
+
+
+if __name__ == '__main__':
+	pad(sys.argv[1:])
