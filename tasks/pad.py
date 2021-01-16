d148fb36f0c1d891a2f8c2e6619836c73600a6af
RaniaKol <75841196+RaniaKol@users.noreply.github.com>
2021-01-16 21:14:37 +0100

    Update pad.py

--- tasks/pad.py
+++ tasks/pad.py
@@ -3,35 +3,25 @@ import numpy as np
 
 
 def pad(input_list):
-	"""
-input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
-return a matrix where
-1) matrix[i][:len(input_list[i])] == input_list[i]
-and
-2) matrix[i][len(input_list[i]):] == [0,0, ..., 0]
-... 
-"""
 
+	"""
+	Given a input list of nested list of integers:
 	input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
+	return a matrix where 
+	1) matrix[i][:len(input_list[i])] == input_list[i]
+	and
+	2) matrix[i][len(input_list[i]):] == [0,0, ..., 0]
+	
+	"""
 	y=np.matrix(input_list)
 	length = 2*max(map(len, input_list))
 	matrix = np.matrix([x+[0]*(length-len(x)) for x in input_list], dtype="int")
-
 	#matrix1= np.split(matrix,4)
 	#for i in range(len(input_list)):
 	#	if np.any(matrix1[i][:len(input_list[i])]) == np.any(input_list[i]):
 	#		print(True)
-	return matrix
-	
-					
-
-
-
-    #pass
-
-
-
-
+	return(matrix)
 
 if __name__ == '__main__':
-	pad(sys.argv[1:])
+	input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
+	pad(input_list)
